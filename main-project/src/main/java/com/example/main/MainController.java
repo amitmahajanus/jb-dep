19c99a4d99646a06b3a68e33e3f76de42c0db22e
amitmahajanus <amitmahajanus@gmail.com>
2025-06-17 15:20:03 +0530

    initial checkin - working project

--- main-project/src/main/java/com/example/main/MainController.java
+++ main-project/src/main/java/com/example/main/MainController.java
@@ -4,15 +4,17 @@ import com.example.dependency.MyBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class MainController {
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Autowired
     private MyBean myBean;
 
+    @GetMapping("/mybean")
     public void testBean() {
         if (myBean == null) {
             logger.error("MyBean is null!");
